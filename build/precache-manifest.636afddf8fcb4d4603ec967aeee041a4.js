0a0e6a991dd0a68058d6393caacf5c91ea6ecc8b
navishNSR <54846510+navishNSR@users.noreply.github.com>
2024-01-31 05:11:07 +0530

    Uploaded latest file

---
+++ build/precache-manifest.636afddf8fcb4d4603ec967aeee041a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff125abf21ec1d2e3943ee27f08a3724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600148429c3c064ce71d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "600148429c3c064ce71d",
+    "url": "/static/js/main.d26fec78.chunk.js"
+  },
+  {
+    "revision": "e67b41604717847bfcd7",
+    "url": "/static/js/runtime~main.e0c0d350.js"
+  }
+]);
